278ca4e1b36ebb7880a2a8b0f4c87c9ceb856a2e
ayofalo <ayoflw@gmail.com>
2017-03-26 17:06:00 +0100

    Third commit

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,57 +1,49 @@
-var readLine = require("readline");
-if (process.platform === "win32") { /*process as a property that stores pltform type*/
-  var rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.on("SIGINT", function() {
-    process.emit("SIGINT");
-  });
-}
-
 var mongoose = require('mongoose');
+var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
-
 if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI; //process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGODB_URI;
 }
+
 mongoose.connect(dbURI);
-mongoose.connection.on('connected', function() { //Monitoring the connection event
-  console.log('Mongoose connected to' + dbURI);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
 });
 mongoose.connection.on('error', function(err) {
-  console.log('Mongoose connection error:' + err);
+    console.log('Mongoose connection error: ' + err);
 });
 mongoose.connection.on('disconnected', function() {
-  console.log('Monoose disconnected');
+    console.log('Mongoose disconnected');
 });
-gracefulShutdown = function(msg, callback) {
-  mongoose.connection.close(function() {
-    console.log('Mongoose disconnected through' + msg);
-    callback();
-  });
-};
 
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
 // For nodemon restarts
 process.once('SIGUSR2', function() {
-  gracefulShutdown('nodemon restart', function() {
-    process.kill(process.pid, 'SIGUSR2'); // The process.kill is a continuation callback that is executed after the gracefulShutdown is executed
-  });
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
-
-//For app termination
+// For app termination
 process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function() {
-    process.exit(0); /*Even after declaring a function, the process.kill was added but it wasnt originally declared. I find it very strange.*/
-  });
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
 });
 // For Heroku app termination
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function() {
-    process.exit(0);
-  });
-
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
-//I can easily copy this from application  mainly becaue the events i am listening for are always the same. 
-//I only have to change the database connection string. I wold also require it in the app.js mainy so that the connection opens up early on in the application's life. 
+
+// BRING IN YOUR SCHEMAS & MODELS
 require('./locations');
